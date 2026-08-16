9daf9f68f096f26f6e6be2256d867fb4b40c9ef9
Ashikur Rahaman Ahad <ashikurrahamanahad@gmail.com>
2026-08-16 20:26:41 +0600

    server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import connectDB from './config/db.js';
+
+const app = express();
+
+// Middleware
+app.use(cors({ origin: '*', credentials: true }));
+app.use(express.json());
+app.use(async (req, res, next) => {
+  try {
+    await connectDB();
+    next();
+  } catch (err) {
+    res.status(500).json({ message: 'Database Connection Error' });
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('News Portal API Engine Running');
+});
+
+const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+export default app;
